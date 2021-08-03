ac2c1f78698036796a9a5627f5d8d27627eedb64
Pavithran <pavithranthilakanathan@gmail.com>
2021-08-03 20:38:33 +0530

    Scan alphanumeric and identifiers

--- src/com/builderpreter/lox/Scanner.java
+++ src/com/builderpreter/lox/Scanner.java
@@ -13,7 +13,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+    private static final Map<String, TokenType> keywords;
 
+//    Reserved keywords
+    static{
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if",     IF);
+        keywords.put("nil",    NIL);
+        keywords.put("or",     OR);
+        keywords.put("print",  PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super",  SUPER);
+        keywords.put("this",   THIS);
+        keywords.put("true",   TRUE);
+        keywords.put("var",    VAR);
+        keywords.put("while",  WHILE);
+    }
     Scanner(String source){
         this.source = source;
     }
@@ -60,13 +81,78 @@ public class Scanner {
                     addToken(SLASH);
                 }
                 break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"': string(); break;
             default:
-                Lox.error(line, "Unexpected character");
+                if (isDigit(c)) {
+                    number();
+                }else if(isAlpha(c)){
+                    identifier();
+                }
+                else{
+                    Lox.error(line, "Unexpected character");
+                }
                 break;
         }
     }
 
 //    Helper functions
+//    Consume all of the identifier lexeme and add it as a token
+    private void identifier(){
+        while(isAlphanumeric(peek())) advance();
+//        Check if scanned token matches reserved keywords
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+//    Check if character is alphanumeric
+    private boolean isAlpha(char c){
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+    private boolean isAlphanumeric(char c){
+        return isAlpha(c) || isDigit(c);
+    }
+//    Consume number strings
+    private void number(){
+        while(isDigit(peek())) advance();
+
+//        Check for decimal points
+        if(peek() == '.' && isDigit(peekNext())){
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+//    Consume string literals
+    private void string(){
+        while(peek() != '"' && !isAtEnd()){
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd()){
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+//        Consume the closing "
+        advance();
+
+//        Trim the quotes and store the raw literal to token
+        String value = source.substring(start + 1, current + 1);
+        addToken(STRING, value);
+    }
 //    Check if the character matches with the next character
     private boolean match(char expected){
         if(isAtEnd()) return false;
@@ -75,17 +161,26 @@ public class Scanner {
         current++;
         return true;
     }
-//    Return next character 2 characters from the character being swtiched
+//    Return next character from the character currently being checked
     private char peek(){
         if(isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+//    Look ahead another character after peek
+    private char peekNext(){
+        if(current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+//    Check if the character is a number
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
     }
 
 //    Check if at the end of code
     private boolean isAtEnd(){
         return current >= source.length();
     }
-
+//    Consume the current character
     private char advance(){
         return source.charAt(current++);
     }
@@ -93,7 +188,7 @@ public class Scanner {
     private void addToken(TokenType type){
         addToken(type, null);
     }
-
+//    Add token object
     private void addToken(TokenType type, Object literal){
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
